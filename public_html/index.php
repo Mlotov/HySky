7d14c6bf2eea1d0ceabaa17c9f664e8dce77fbca
Mlotov <56455612+Mlotov@users.noreply.github.com>
2019-10-12 11:33:18 +0200

    Add files via upload

---
+++ public_html/index.php
@@ -0,0 +1,18 @@
+<!-- HEADER -->
+
+<?php
+ $_SESSION["title"] = "HySky | Welcome!";
+ require_once("includes/header.php");
+ ?>
+
+<!-- CONTENT -->
+
+    <div class="container d-flex justify-content-center mt-5">
+        <h1>Your #1 and Only Skyblock Website!</h1>
+    </div>
+
+<!-- FOOTER -->
+
+<?php
+require_once("includes/footer.php");
+?>
